4e14ffd4e9d92cb4787e7afd50beb92a6c1bee2c
deiz0n <eduardomacedosilva@outlook.com>
2022-12-26 18:47:47 -0300

    Criação do projeto

---
+++ src/test/java/com/deiz0ndev/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deiz0ndev.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
