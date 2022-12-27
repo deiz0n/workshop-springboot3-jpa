0a9d5d08d4f7e6117de7f8241e1acd8822210890
deiz0n <eduardomacedosilva@outlook.com>
2022-12-27 10:51:00 -0300

    Banco de dados H2, perfil de teste, JPA

--- src/main/java/com/deiz0ndev/curso/entidades/Usuario.java
+++ src/main/java/com/deiz0ndev/curso/entidades/Usuario.java
@@ -3,9 +3,17 @@ package com.deiz0ndev.curso.entidades;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
